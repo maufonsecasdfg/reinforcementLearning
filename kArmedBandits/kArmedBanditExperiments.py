f0a6d06b4810febdf453936155a1436ec7a3c64e
maufonsecasdfg <maufonseca@gmail.com>
2021-10-12 21:52:55 -0600

    kArmedBandit adapted the experiements function to work with new agent

--- kArmedBandits/kArmedBanditExperiments.py
+++ kArmedBandits/kArmedBanditExperiments.py
@@ -6,7 +6,8 @@ from kArmedBandit import *
 from scipy.stats import norm
 
 
-def run_experiment(runs, steps, k, bandit_type, reward_mean, reward_std, arm_dist_std, nonstat_bandit_rand_walk_update_std, agent_method, agent_epsilon, agent_alpha):
+def run_experiment(runs, steps, k, bandit_type, true_reward_mean, true_reward_std, arm_dist_std, nonstat_bandit_rand_walk_update_std, 
+                    agent_method, agent_alpha, agent_Q_1=None, agent_epsilon=None, agent_c=None):
     results = pd.DataFrame()
     experiment_id = datetime.now().strftime("%Y%m%d%H%M%S")
     experiment_parameters = pd.DataFrame(pd.Series({
@@ -15,23 +16,25 @@ def run_experiment(runs, steps, k, bandit_type, reward_mean, reward_std, arm_dis
         'steps' : steps,
         'k' : k,
         'bandit_type' : bandit_type,
-        'reward_mean' : reward_mean,
-        'reward_std' : reward_std,
+        'true_reward_mean' : true_reward_mean,
+        'true_reward_std' : true_reward_std,
         'arm_dist_std' : arm_dist_std,
         'nonstat_bandit_rand_walk_update_std' : nonstat_bandit_rand_walk_update_std,
         'agent_method' : agent_method,
         'agent_epsilon' : agent_epsilon,
-        'agent_alpha' : agent_alpha
+        'agent_alpha' : agent_alpha,
+        'agent_Q_1' : str(agent_Q_1),
+        'agent_c' : agent_c
     })).transpose()
 
     for j in range(runs):
         distributions = [norm]*k
-        dist_params = [[norm(reward_mean,reward_std).rvs(),arm_dist_std] for i in range(k)]
+        dist_params = [[norm(true_reward_mean,true_reward_std).rvs(),arm_dist_std] for i in range(k)]
         if bandit_type == 'stationary':
             arms = kArmedBandit(k=k, distributions=distributions, dist_params=dist_params)
         elif bandit_type == 'non-stationary':
             arms = kArmedBanditNonStationary(k=k, distributions=distributions, dist_params=dist_params, rand_walk_update_std=nonstat_bandit_rand_walk_update_std)
-        agent = Agent(k = k, method=agent_method, epsilon=agent_epsilon,  alpha=agent_alpha)
+        agent = Agent(k = k, method=agent_method, alpha=agent_alpha, Q_1=agent_Q_1, epsilon=agent_epsilon, c=agent_c)
         runner = kArmedBanditRun(arms, agent, steps=steps)
         runner.run()
         
